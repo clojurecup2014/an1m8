1195f284e93f943eafe7bf61d45c31bc7c2fc2bd
Andrew Sernyak <ndrew.sernyak@gmail.com>
2014-09-27 06:58:11 +0300

    WIP on frontend

--- frontend/project.clj
+++ frontend/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2311"]]
+                 [org.clojure/clojurescript "0.0-2322"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [org.clojure/google-closure-library "0.0-20140718-946a7d39"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
