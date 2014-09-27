7832f34b2a963ca1a69ed0fb3a34950ec46318e1
Igor.Lushchyk <ihor.lushchyk@sethq.com>
2014-09-27 05:29:51 +0300

    deleted

--- an1m8/src/an1m8/util.clj
+++
@@ -1,8 +0,0 @@
-(ns an1m8.util
-  (:require [noir.io :as io]
-            [markdown.core :as md]))
-
-(defn md->html
-  "reads a markdown file from public/md and returns an HTML string"
-  [filename]
-  (md/md-to-html-string (io/slurp-resource filename)))
